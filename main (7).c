89aa627cfeff52ffbab2caf0cf1b0ebf03d8a89f
SCOV2R <138464178+SCOV2R@users.noreply.github.com>
2023-07-03 13:51:36 +0300

    Add files via upload

---
+++ main (7).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int t1, t2, t3;
+    float total_time;
+
+    printf("ВЕДІТЬ ТРИ ЗНАЧЕННЯ\n");
+    scanf("%d %d %d", &t1, &t2, &t3);
+
+    total_time = t1 + t2 + t3;
+
+    printf("Загальний час, необхідний для з'їдання пирога: %.2f годин\n", total_time);
+
+    return 0;
+}
